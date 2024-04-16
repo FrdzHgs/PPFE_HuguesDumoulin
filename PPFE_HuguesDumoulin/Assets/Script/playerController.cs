fb575668e774ccb8b6d312d2b7f23fdbe91c2452
FrdzHgs <dumoulin77hugues@gmail.com>
2024-04-16 18:44:13 +0200

    controller

--- PPFE_HuguesDumoulin/Assets/Script/playerController.cs
+++ PPFE_HuguesDumoulin/Assets/Script/playerController.cs
@@ -7,16 +7,21 @@ public class playerController : MonoBehaviour
 {
     private float xSpeed;
     private float zSpeed;
-    private Rigidbody rb;
+    private float speed;
+    private Vector2 direction;
+    private Vector3 movement;
+    
+    private bool isGrounded = false;
 
     public Transform zRotateBonhomme;
     public Transform xRotateBonhomme;
+    public float gravity;
 
     private float DirectionAngle = 0f;
 
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        
     }
 
     void Update()
@@ -27,38 +32,44 @@ public class playerController : MonoBehaviour
         }
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Block")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Block")
+        {
+            isGrounded = false;
+        }
+    }
+
     void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.Mouse0))
+        if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.15 || Mathf.Abs(Input.GetAxis("Vertical")) > 0.15)
         {
-            xSpeed += Input.GetAxis("Mouse Y");
-            zSpeed += Input.GetAxis("Mouse X");      
-            if(xSpeed >= 1)
-            {
-                xSpeed = 1;
-            }
-            if(xSpeed <= -1)
-            {
-                xSpeed = -1;
-            }
-            if(zSpeed >= 1)
-            {
-                zSpeed = 1;
-            }
-            if(zSpeed <= -1)
-            {
-                zSpeed = -1;
-            }
+            xSpeed = Input.GetAxis("Horizontal");
+            zSpeed = Input.GetAxis("Vertical");    
+            direction = new Vector2(-zSpeed, -xSpeed).normalized;  
+            speed = Vector2.ClampMagnitude(new Vector2(-zSpeed, -xSpeed), 1).magnitude;                 
         }
         else
         {
             xSpeed = 0;
             zSpeed = 0;
+            direction = new Vector2(0,0); 
         }
 
-        rb.velocity = new Vector3(xSpeed, rb.velocity.y, -zSpeed);
-        DirectionAngle = Mathf.Atan2(-xSpeed, zSpeed) * Mathf.Rad2Deg;
-        zRotateBonhomme.eulerAngles = new Vector3(0, DirectionAngle, 0);
-        xRotateBonhomme.localEulerAngles = new Vector3(-rb.velocity.magnitude*30, 0, 0);  
+        movement = new Vector3(direction.x, isGrounded ? 0 : -gravity, direction.y) * speed;
+        this.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
+
+        Debug.Log(direction);
+        // DirectionAngle = Mathf.Atan2(-xSpeed, zSpeed) * Mathf.Rad2Deg;
+        // transform.eulerAngles = new Vector3(0, DirectionAngle, 0);
+        //xRotateBonhomme.localEulerAngles = new Vector3(-rb.velocity.magnitude*30, 0, 0);  
     }
 }
